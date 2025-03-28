764c155652fd3a7d22f32564d2d4d648e95298f5
Lori Ramey <loriramey@hotmail.com>
2025-03-28 16:34:13 -0400

    adjust LFS

--- pages/DataViz.py
+++ pages/DataViz.py
@@ -2,16 +2,39 @@
 
 import streamlit as st
 
+def display_info():
+    st.title("Data Overview")
+    st.markdown("""
+            **Play Next** runs on data obtained through a public Kaggle dataset (read more on the About page)
+             which was originally pulled from BoardGameGeek.com's public API in early 2025. The data set
+             contains 25,700 games after cleaning to remove entries missing information in major fields. 
+             
+             \n\n  You can explore the data via the visuals and interactive controls below.
+    """)
+    pass
+
 def display_static_visuals():
     """Display static visual assets (e.g., PNG images) on the page."""
     st.title("Data Visualizations")
-    st.subheader("Static Visuals")
-    # Stub: Code to display images from the visualizations folder
+    st.header("Static Visuals")
+    st.subheader("Games Published Each Year Since 1980")
+    st.write("Note the scale for the left axis is thousands")
+    st.image("visualizations/games_per_year.png", caption="Games Per Year, in Thousands")
+    st.markdown("""---""")
+    st.subheader("Distribution of Ratings in the Data Set")
+    st.write("Here's how the average ratings are distributed across the data set.")
+    st.image("visualizations/rating_distribution.png", caption="Distribution of Ratings in the Data Set")
+    st.write("These ratings are a Bayesian average which moderates the influence of games with only a few reviews.")
+    st.markdown("""---""")
+    st.subheader("Average Bayesian Rating per Publication Year since 1980")
+    st.write("Here is the average Bayesian rating of games for each year since 1980.")
+    st.image("visualizations/average_bayesaverage_per_year.png", caption="Average Rating for Each Year")
+    st.markdown("""---""")
     pass
 
 def display_interactive_charts():
     """Display interactive visualizations (e.g., Plotly charts) on the page."""
-    st.subheader("Interactive Visualizations")
+    st.header("Interactive Visualizations")
     # Stub: Code to generate or embed interactive charts
     pass
 
